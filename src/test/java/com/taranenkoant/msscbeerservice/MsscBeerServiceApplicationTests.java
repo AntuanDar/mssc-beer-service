e73c68b5ff4cd04816c9e597dee46ab84077e534
antuan_dar <antuan_dar@bk.ru>
2021-07-04 16:33:03 +0300

    initial commit. Closes #1

---
+++ src/test/java/com/taranenkoant/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taranenkoant.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
