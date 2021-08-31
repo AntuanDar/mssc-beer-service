7bbc0f00aac4e0725f28ce5c39dbbb116830b095
Anton <antuan_dar@bk.ru>
2021-08-31 22:09:24 +0300

    closes #10

---
+++ src/main/java/com/taranenkoant/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.taranenkoant.msscbeerservice.web.mappers;
+
+import com.taranenkoant.msscbeerservice.domain.Beer;
+import com.taranenkoant.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
